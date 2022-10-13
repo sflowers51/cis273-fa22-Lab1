fca5969036d31587e5627879bb6639850def4d77
sflowers51 <111446369+sflowers51@users.noreply.github.com>
2022-10-13 09:38:09 -0500

    Update Program.cs

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -3,7 +3,33 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        LinkedList<string> linkedList = new LinkedList<string>();
+
+        linkedList.AddLast("xbx");
+        linkedList.AddLast("pka");
+        linkedList.AddLast("pka");
+        linkedList.AddLast("xbx");
+
+        var reverse = linkedList.Reverse();
+        var currentNode = linkedList.First;
+        var currentNodeReverse = reverse.First();
+
+        while( currentNode != null)
+        {
+            currentNode = currentNode.Next;
+        }
+
+        foreach( var node in linkedList)
+        {
+            Console.WriteLine(node);
+        }
+    }
+
+    public static bool IsPalindrome<T>(LinkedList<T> linkedList)
+    {
+        // are the first and alst items the same?
+        // if so, move toward the middle
+        return true;
     }
 }
 
